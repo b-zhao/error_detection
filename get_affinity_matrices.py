f3bebc562f50c83019c628e44e4efffd42672b91
lusean <seanlu97@yahoo.com>
2019-12-07 03:08:12 -0500

    Add new aff matrix function

--- get_affinity_matrices.py
+++ get_affinity_matrices.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from get_features import *
 import time
+import py_entitymatching as em
 
 '''
 Return affinity matrix A. A[i][j][f] = (t_i[f] == t_j[f])
@@ -20,6 +21,21 @@ def affinity_matrix_binary(filename):
 
     return affinity_matrix
 
+def affinity_matrix_similarity(filename):
+    df = pd.read_csv(filename)
+    df, _, _ = remove_na(df)
+    X = np.array(df)
+
+    num_rows, num_cols = np.shape(X)
+    affinity_matrix = np.zeros((num_rows, num_rows, num_cols))
+    
+    for i in range(int(num_rows)):
+        row_i_tile = np.tile(np.array(X[i]), (num_rows, 1))
+        for j in range(int(num_rows)):
+            for k in range(int(num_cols)):
+                affinity_matrix[i][j][k] = em.lev_dist(X[j][k], row_i_tile[j][k])
+
+    return affinity_matrix
 
 # start_time = time.time()
 # affinity_matrix = affinity_matrix_binary("data/hospital.csv")
